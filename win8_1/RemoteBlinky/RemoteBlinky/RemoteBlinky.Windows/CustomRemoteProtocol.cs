455896ea202b7f8818177ed6cdf134f17447d2c6
JamesCaska <jamescaska@virtualbreadboard.com>
2015-07-20 15:14:59 +0200

    created to demonstrate creating custom protocol

---
+++ win8_1/RemoteBlinky/RemoteBlinky/RemoteBlinky.Windows/CustomRemoteProtocol.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maker.Serial;
+namespace RemoteBlinky
+{
+    public class CustomRemoteProtocol
+    {
+        IStream connection;
+        public CustomRemoteProtocol(IStream connection)
+        {
+            this.connection = connection;
+        }
+
+        public bool LEDOn {
+            set
+            {
+                if (value)
+                {
+                    connection.write((byte)'H');
+                }
+                else
+                {
+                    connection.write((byte)'L');
+                }
+
+               
+            }
+        }
+ 
+    }   
+  
+}
